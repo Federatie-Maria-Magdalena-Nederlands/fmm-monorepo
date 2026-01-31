058631b1c3e93adccfd613e286aea54c1fa5cd83
Seiji Ralph Villafranca <seiji.ralphvillafranca+kpn@kpn.com>
2026-01-31 22:34:22 +0100

    added correct gallery

--- apps/lourdes-website/src/app/pages/home/sections/join-community-section/join-community-section.ts
+++ apps/lourdes-website/src/app/pages/home/sections/join-community-section/join-community-section.ts
@@ -1,8 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-join-community-section',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './join-community-section.html',
 })
-export class JoinCommunitySection {}
+export class JoinCommunitySection {
+  public router = inject(Router);
+
+  public joinUs() {
+    this.router.navigate(['/volunteer']);
+  }
+
+}
